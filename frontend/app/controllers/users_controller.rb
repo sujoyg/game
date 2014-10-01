f3cd11ef10028d6a929efc51116b25fe02b567f0
Sujoy Gupta <sujoy.gupta@appdirect.com>
2014-10-01 14:44:01 -0700

    Connect to the API to retrieve users.

--- frontend/app/controllers/users_controller.rb
+++ frontend/app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
+require 'lib/api'
+
 class UsersController < ApplicationController
   before_filter { |c| c.send :authorize, root_path }
 
   def home
+    @people = JSON.parse(API.get 'users').map { |person| OpenStruct.new person }
   end
 end
